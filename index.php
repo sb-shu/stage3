2cc8146897033615034cb2cb98e6fa63da1660a2
xclwright <xclwright@gmail.com>
2022-03-16 15:22:58 +0000

    Dividing page

--- index.php
+++ index.php
@@ -1,35 +1,23 @@
 <html>
 <head>
+  <link rel="stylesheet" href="style.css" />
 </head>
 <body>
 
 <div class="split left">
   <div class="centered">
-    <img src="img_avatar2.png" alt="Avatar woman">
+    <img src="Donny.webp" alt="Profile Picture">
     <h2>Jane Flex</h2>
-    <p>Some text.</p>
+    <p>This is the profile</p>
   </div>
 </div>
 
 <div class="split right">
   <div class="centered">
-    <img src="img_avatar.png" alt="Avatar man">
-    <h2>John Doe</h2>
+    <h2>This is the artefacts</h2>
     <p>Some text here too.</p>
   </div>
 </div>
-
-<div class="topnav">
-  <a class="active" href="index.php">Home</a>
-  <a>Register</a>
-  <a>Update</a>
-  <a>Admin</a>
-</div>
-
-<div style="padding-left:16px">
-  <h2>Welcome Student Portfolio Creation Tool</h2>
-     <h2>Group 30</h2> 
-  </p>
 
 </body>
 </html>
