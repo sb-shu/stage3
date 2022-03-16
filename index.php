f5d5c905f8d9607772f7a4cf5d44e0040ca082d2
xclwright <xclwright@gmail.com>
2022-03-16 15:00:24 +0000

    test

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-Hello world and James
+<html>
+<head>
+</head>
+<body>
+
+<div class="split left">
+  <div class="centered">
+    <img src="img_avatar2.png" alt="Avatar woman">
+    <h2>Jane Flex</h2>
+    <p>Some text.</p>
+  </div>
+</div>
+
+<div class="split right">
+  <div class="centered">
+    <img src="img_avatar.png" alt="Avatar man">
+    <h2>John Doe</h2>
+    <p>Some text here too.</p>
+  </div>
+</div>
+
+<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a>Register</a>
+  <a>Update</a>
+  <a>Admin</a>
+</div>
+
+<div style="padding-left:16px">
+  <h2>Welcome Student Portfolio Creation Tool</h2>
+     <h2>Group 30</h2> 
+  </p>
+
+</body>
+</html>
