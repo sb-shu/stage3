57eb0881db899500e59cd368bba799b4937d2316
James Burdett <james211902@gmail.com>
2022-03-24 00:12:13 +0000

    -Rewrote a lot so it makes a bit more sense. Reverting some changes smade previously.

--- DatabaseInterface.php
+++ DatabaseInterface.php
@@ -1,5 +1,4 @@
 <!--
-    Edited by: James Burdett
     Last edited: 23/03/2022
 -->
 
@@ -24,10 +23,48 @@ class PortfolioArtefact{
     public $FileLink; // String
     public $Tags; // String[]
 
+    private function __constructor(){
+        
+    }
+
+    /* #region Create, Get, and Save */
+
+    static function CreateArtefact() : ?PortfolioArtefact{
+        // TODO: Create the artefact in the database.
+    }
+
+    static function GetArtefact($artefactID) : ?PortfolioArtefact {
+        // Temporary code
+        $artefact = new PortfolioArtefact();
+        $artefact->ID = $artefactID;
+        $artefact->FileLink = "";
+        $artefact->ThumbnailLink = "";
+        $artefact->Tags = ["Year1","University"];
+        return $artefact;
+    
+        $statement = $db->prepare("SELECT ThumbnailLink, FileLink, Tags FROM UserAccounts WHERE ID = :id");
+        $statement->bindParam(":id", $artefactID);
+        $result = $statement->execute()->fetchArray(SQLITE3_ASSOC);
+    
+        // If query returns empty, return.
+        if (!$result) {
+            return null;
+        }
+    
+        $artefact = new UserAccount();
+        $artefact->ID = $artefactID;
+        $artefact->FileLink = $result["FileLink"];
+        $artefact->ThumbnailLink = $result["ThumbnailLink"];
+        $artefact->Tags = explode("\0",$result["Tags"]);
+        return $artefact;
+    }
+
     // Saves changes to this object to database.
     function SaveChanges(){
         // TODO: Write fields to database.
     }
+
+    /* #endregion */
 }
 
 // Class that holds data about a person's past work experience.
@@ -38,10 +75,18 @@ class PortfolioWorkExperience{
     public $JobTitle; // String
     public $Description; // String
 
+    private function __constructor(){
+        
+    }
+
+    /* #region Create, Get, and Save */
+
     // Saves changes to this object to database.
     function SaveChanges(){
         // TODO: Write fields to database.
     }
+
+    /* #endregion */
 }
 
 // Class that holds all publicly accessable data regarding a user, including name, portfolio items, and more.
@@ -52,6 +97,59 @@ class UserAccount{
     public $ProfilePictureLink; // String
     public $AboutMe; // String
     public $Contacts; // String[]
+
+    private function __constructor(){
+
+    }
+
+    /* #region Create, Get, and Save */
+
+    static function CreateUserAccount($desiredUsername, $password) : ?UserAccount{
+        $db = new SQLite3(SQLPATH);
+
+        // Check if username already exists...
+        if(DoesUserExist($desiredUsername))
+            return null;
+
+        // If requirements are met, create and return true.
+
+        $statement = $db->prepare("INSERT INTO UserAccounts (Username, PasswordHash) VALUES (:username, :passwordHash)");
+        $statement->bindParam(":username", $desiredUsername);
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $statement->bindParam(":passwordHash", $hash);
+        $statement->execute();
+        return UserAccount::GetUserAccount($desiredUsername);
+    }
+
+    static function GetUserAccount($username) : ?UserAccount{
+        $db = new SQLite3(SQLPATH);
+
+        $statement = $db->prepare("SELECT FirstName, LastName, Contacts, ProfilePictureLink, AboutMeText FROM UserAccounts WHERE Username = :username");
+        $statement->bindParam(":username", $username);
+        $result = $statement->execute()->fetchArray(SQLITE3_ASSOC);
+
+        // If query returns empty, return.
+        if (!$result) {
+            return null;
+        }
+
+        $userObject = new UserAccount();
+        $userObject->Username = $username;
+        $userObject->FirstName = $result["FirstName"];
+        $userObject->LastName = $result["LastName"];
+        $userObject->ProfilePictureLink = $result["ProfilePictureLink"];
+        $userObject->AboutMe = $result["AboutMeText"];
+        $userObject->Contacts = explode("\0",$result["Contacts"]);
+
+        return $userObject;
+    }
+
+    // Saves changes to this object to database.
+    function SaveChanges(){
+        // TODO: Write fields to database.
+    }
+
+    /* #endregion */
 
     function GetEducation(){
         // TODO: Fetch from DB.
@@ -65,24 +163,19 @@ class UserAccount{
         // TODO: Fetch from DB.
     }
 
+    // Creates a new artefact attached to this account.
     function AddNewArtefact() : PortfolioArtefact{
         $db = new SQLite3(SQLPATH);
         $statement = $db->prepare("INSERT INTO PortfolioArtefacts (Username) VALUES (:username)");
         $statement->bindParam(":username", $this->Username);
-        var_dump($statement->getSQL());
         $statement->execute();
-        var_dump($db->lastInsertRowid());
 
-        return GetArtefact($db->lastInsertRowid());
+        return PortfolioArtefact::GetArtefact($db->lastInsertRowid());
     }
 
+    // Returns all artefacts attached to this account.
     function GetArtefacts(){
         // TODO: Fetch from DB.
-    }
-
-    // Saves changes to this object to database.
-    function SaveChanges(){
-        // TODO: Write fields to database.
     }
 }
 
@@ -90,54 +183,25 @@ class UserAccount{
 
 /* #region User Functions */
 
-// Creates a UserAccount. Returns true if successful.
-function CreateUser($desiredUsername, $password) : bool{
-    $db = new SQLite3(SQLPATH);
-
-    // Check if username already exists...
-    $statement = $db->prepare("SELECT Username FROM UserAccounts WHERE Username = :username");
-    $statement->bindParam(":username", $desiredUsername);
-    $result = $statement->execute()->fetchArray();
-    if($result)
-        return false;
-
-    // If requirements are met, create and return true.
-
-    $statement = $db->prepare("INSERT INTO UserAccounts (Username, PasswordHash) VALUES (:username, :passwordHash)");
-    $statement->bindParam(":username", $desiredUsername);
-    $hash = password_hash($password, PASSWORD_DEFAULT);
-    $statement->bindParam(":passwordHash", $hash);
-    $statement->execute();
-    return true;
+// Creates a UserAccount. Returns a UserAccount object if successful
+function CreateUser($desiredUsername, $password) : ?UserAccount{
+    return UserAccount::CreateUserAccount($desiredUsername, $password);
 }
 
 // Gets a user from the database with a given username.
-function GetUser($username) : UserAccount{
-    $db = new SQLite3(SQLPATH);
-
-    $statement = $db->prepare("SELECT FirstName, LastName, Contacts, ProfilePictureLink, AboutMeText FROM UserAccounts WHERE Username = :username");
-    $statement->bindParam(":username", $username);
-    $result = $statement->execute()->fetchArray(SQLITE3_ASSOC);
-
-    // If query returns empty, return.
-    if (!$result) {
-        return null;
-    }
-
-    $userObject = new UserAccount();
-    $userObject->Username = $username;
-    $userObject->FirstName = $result["FirstName"];
-    $userObject->LastName = $result["LastName"];
-    $userObject->ProfilePictureLink = $result["ProfilePictureLink"];
-    $userObject->AboutMe = $result["AboutMeText"];
-    $userObject->Contacts = explode("\0",$result["Contacts"]);
-
-    return $userObject;
+function GetUser($username) : ?UserAccount{
+    return UserAccount::GetUserAccount($username);
 }
 
-// Deletes a user from the database with a given username.
-function DeleteUser($username){
-    // TODO : Delete from database.
+// Runs a query to check if a username is in use.
+function DoesUserExist($username) : Bool{
+    $db = new SQLite3(SQLPATH);
+    $statement = $db->prepare("SELECT Username FROM UserAccounts WHERE Username = :username");
+    $statement->bindParam(":username", $username);
+    $result = $statement->execute()->fetchArray();
+    if($result)
+        return true;
+    return false;
 }
 
 // Compares a user's password. Returns true if the passwords match.
@@ -157,7 +221,6 @@ function UserComparePassword($username, $password):bool{
 }
 
 /* #endregion */
-
 
 // Initializes tables
 function InitializeDatabase(){
@@ -229,10 +292,10 @@ function InitializeDatabase(){
 InitializeDatabase();
 
 // Create a testing account.
-if(CreateUser("testuser", "pass")){
-    $user = GetUser("testuser");
+$newAccount = CreateUser("testuser", "pass");
+if($newAccount){
     for($i = 0; $i < 5; $i++){
-        $artefact = $user->AddNewArtefact();
+        $artefact = $newAccount->AddNewArtefact();
     }
 }
 ?>
